c686e0ba93ef2e173ec5eaf9b19a9950dffb840c
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-09-09 00:45:11 +0600

    add basic toll calling

---
+++ day8.py
@@ -0,0 +1,10 @@
+def calculator(a, b):
+    return a + b
+
+
+def get_weather(city):
+    return f"Weather information for {city}"
+
+
+def search_web(query):
+    return f"Search results for {query}"
